d288df819588d1731c54145ed212e1ff307b7751
RPI Roshan <yroshan630@gmail.com>
2025-03-24 18:08:11 +0530

    int max count

--- rpi_spi.m
+++ rpi_spi.m
@@ -8,13 +8,21 @@ spiDev = spidev(rpi, 'CE1', 0, 32e6); % Using CE1 (chip select 1)
 
 % Define the number of bytes to read
 bytesToRead = 1024;  % Match BUFFER_SIZE from Teensy
+maxBytesPerTransaction = 256; % Maximum bytes per transaction
 
 % Create a buffer for dummy write data (required for SPI reads)
-dummyData = zeros(1, bytesToRead, 'uint8');
+dummyData = zeros(1, maxBytesPerTransaction, 'uint8');
 
-% Read data from the Teensy
+% Initialize the received data array
+receivedData = zeros(1, bytesToRead, 'uint8');
+
+% Read data from the Teensy in chunks
 fprintf('Reading %d bytes from SPI...\n', bytesToRead);
-receivedData = writeRead(spiDev, dummyData);
+for i = 1:(bytesToRead / maxBytesPerTransaction)
+    startIdx = (i-1) * maxBytesPerTransaction + 1;
+    endIdx = i * maxBytesPerTransaction;
+    receivedData(startIdx:endIdx) = writeRead(spiDev, dummyData);
+end
 
 % Display the first few and last few bytes
 fprintf('First 10 bytes: ');

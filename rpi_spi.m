8546f88f1831424f093fdf99683a60b1d2358124
Roshan Yadav <56498395+roshanyadav29@users.noreply.github.com>
2025-03-24 17:06:56 +0530

    teensy compatible sketch

--- rpi_spi.m
+++ rpi_spi.m
@@ -4,13 +4,38 @@
 rpi = raspi();
 
 % Create an SPI device object
-spiDev = spidev(rpi, 'CE1', 0, 32e6); % Use 'CE0' for chip select 0
+spiDev = spidev(rpi, 'CE1', 0, 32e6); % Using CE1 (chip select 1)
 
-% Send a command to request data (this depends on your SPI slave setup)
-data = writeRead(spiDev, uint8(0x01));
+% Define the number of bytes to read
+bytesToRead = 1024;  % Match BUFFER_SIZE from Teensy
 
-% Display the received data
-disp(['Received data: ', num2str(data)]);
+% Create a buffer for dummy write data (required for SPI reads)
+dummyData = zeros(1, bytesToRead, 'uint8');
+
+% Read data from the Teensy
+fprintf('Reading %d bytes from SPI...\n', bytesToRead);
+receivedData = writeRead(spiDev, dummyData);
+
+% Display the first few and last few bytes
+fprintf('First 10 bytes: ');
+disp(receivedData(1:10));
+fprintf('Last 10 bytes: ');
+disp(receivedData(end-9:end));
+
+% Calculate timestamp from last 4 bytes (if using the format from Teensy)
+timestamp = uint32(receivedData(end-3)) * 2^24 + ...
+           uint32(receivedData(end-2)) * 2^16 + ...
+           uint32(receivedData(end-1)) * 2^8 + ...
+           uint32(receivedData(end));
+fprintf('Timestamp: %d ms\n', timestamp);
+
+% Plot the data
+figure;
+plot(1:length(receivedData), receivedData, 'b.-');
+title('Data received from Teensy 4.1');
+xlabel('Sample Index');
+ylabel('Sample Value');
+grid on;
 
 % Clean up
 clear spiDev;

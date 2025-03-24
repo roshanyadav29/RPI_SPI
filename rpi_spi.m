2a8856859a769574df48efec6f0c2f42bbeacdf9
Roshan Yadav <56498395+roshanyadav29@users.noreply.github.com>
2025-03-24 18:16:49 +0530

    simpler approach

--- rpi_spi.m
+++ rpi_spi.m
@@ -30,13 +30,6 @@ disp(receivedData(1:10));
 fprintf('Last 10 bytes: ');
 disp(receivedData(end-9:end));
 
-% Calculate timestamp from last 4 bytes (if using the format from Teensy)
-timestamp = uint32(receivedData(end-3)) * 2^24 + ...
-           uint32(receivedData(end-2)) * 2^16 + ...
-           uint32(receivedData(end-1)) * 2^8 + ...
-           uint32(receivedData(end));
-fprintf('Timestamp: %d ms\n', timestamp);
-
 % Plot the data
 figure;
 plot(1:length(receivedData), receivedData, 'b.-');
